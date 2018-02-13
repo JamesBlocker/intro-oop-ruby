d2d94824fdd73cb3258a14c01dbf33816e8ac9f0
Byron <jamesblocker81@gmail.com>
2018-02-13 11:59:45 -0800

    create human file

---
+++ human.rb
@@ -0,0 +1,12 @@
+class Human
+    def initialize(name, color, programmer, languages, pro_languages)
+        @name = name
+        @hair_color = color
+        @programmer = programmer
+        @languages = languages
+        @pro_languages = pro_languages
+    end
+
+    
+
+end

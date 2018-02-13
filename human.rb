f0980952b8aeca9e509992ed410c4a4512488496
Byron <jamesblocker81@gmail.com>
2018-02-13 12:19:33 -0800

    add in human functions for wutang

--- human.rb
+++ human.rb
@@ -7,6 +7,40 @@ class Human
         @pro_languages = pro_languages
     end
 
-    
+    def say_hi
+        p "Hi my name is #{@name}."
+    end
+
+    def are_you_pro
+        p "It is #{@programmer} that I am a programmer"
+    end
+
+    def what_langs
+        print "I speak " 
+        puts @languages.join(" & ")
+        if @programmer
+            print "I also program using "
+            puts @pro_languages.join(" & ")
+        end
+    end
+
+    def fire_all
+        self.say_hi
+        self.are_you_pro
+        self.what_langs
+    end
 
 end
+
+p "X" * 45
+james = Human.new("James", "black", true, ["English", "Spanish"], ["JS", "Ruby"])
+james.fire_all
+p "*" * 30
+
+susan = Human.new("Susan", "brown", false, ["English"], ["dreams", "wishes"])
+susan.fire_all
+p "*" * 30
+
+wutang = Human.new("WuTang Mutha Fucka", "black", true, ["English", "Spanish", "Mandarin"], ["Java", "JS", "PHP", "C#"])
+wutang.fire_all
+p "*" * 30
